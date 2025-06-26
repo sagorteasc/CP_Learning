faeaaf58826142f34b0c5fdd0eb470b23b5f3062
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2025-06-26 11:30:42 +0600

    Sheet 1 - B_Basic_Data_Types

---
+++ ICPC/Sheet 1/B_Basic_Data_Types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main () {
+    int i;
+    long long l;
+    char c;
+    float f;
+    double d;
+    cin >> i >> l >> c >> f >> d;
+    cout << i << "\n" << l << "\n" << c << "\n" << f << "\n" << d;
+}
