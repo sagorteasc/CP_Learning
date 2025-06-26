f15e0fadb210f44fe085c31c1e2443898dcf5a64
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2025-06-26 11:39:17 +0600

    Sheet 1 - F_Digits_Summation

---
+++ ICPC/Sheet 1/F_Digits_Summation.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    long long A, B, C, D, sum;
+    cin >> A >> B;
+    C = (A%10);
+    D = (B%10);
+    sum  = C + D;
+    cout << sum << endl;
+}
