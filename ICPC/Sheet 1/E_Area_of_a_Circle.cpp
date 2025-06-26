c7c31f8fb84ac754404e83b24e02e76e122f2c6a
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2025-06-26 11:37:44 +0600

    Sheet 1 - E_Area_of_a_Circle

---
+++ ICPC/Sheet 1/E_Area_of_a_Circle.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    double R, Area;
+    const double pi = 3.141592653;
+    cin >> R;
+    Area = pi * R * R;
+    cout << fixed << setprecision(9) << Area;
+}
