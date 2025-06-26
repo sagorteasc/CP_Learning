a6e11ffdc4b52bc5286b13820b81a91c497a278a
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2025-06-26 11:34:32 +0600

    Sheet 1 - D_Difference

---
+++ ICPC/Sheet 1/D_Difference.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    long long A, B, C, D;
+    string name = "Difference";
+    cin >> A >> B >> C >> D;
+    cout << name << " = " << (A * B) - (C * D);
+}
