9e184fdfce27f50cacf82339c49ec4e3a08b75a5
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2025-06-27 14:52:09 +0600

    Sheet 1 - L_The_Brothers

---
+++ ICPC/Sheet 1/L_The_Brothers.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    string F1, F2, S1, S2;
+    cin >> F1 >> S1 >> F2 >> S2;
+    if (S1 == S2){
+        cout << "ARE Brothers";
+    }
+    else{
+        cout << "NOT";
+    }
+}
