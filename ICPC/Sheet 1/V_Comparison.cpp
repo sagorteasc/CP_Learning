2f2995c80c58bdd10eb17d1a802ff2dddaadc859
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2025-07-01 00:17:42 +0600

    Sheet 1 - V_Comparison

---
+++ ICPC/Sheet 1/V_Comparison.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int a, b;
+    char s; cin >> a >> s >> b;
+    if (s == '<' && a < b){
+       cout << "Right" << endl;
+    }
+    else if (s == '>' && a > b){
+        cout << "Right" << endl;
+    }
+    else if (s == '=' && a == b){
+        cout << "Right" << endl;
+    }
+    else{
+        cout << "Wrong" << endl;
+    }
+    return 0;
+}
