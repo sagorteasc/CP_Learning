4239fa1d28d653014f3eb235a2b739b8a2ec5e20
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2025-07-08 18:22:08 +0600

    Sheet 2 - D_Fixed_Password

---
+++ ICPC/Sheet 2/D_Fixed_Password.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int x;
+    while (cin >> x){
+        if (x != 1999){
+            cout << "Wrong" << endl;
+        }
+        else{
+            cout << "Correct" << endl;
+            break;
+        }
+    }
+    return 0;
+}
