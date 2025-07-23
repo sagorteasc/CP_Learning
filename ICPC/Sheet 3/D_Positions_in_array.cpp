5893282084782bb6c52282f90484ce730de4c4df
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2025-07-23 11:22:36 +0600

    Sheet 3 - D_Positions_in_array

---
+++ ICPC/Sheet 3/D_Positions_in_array.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int n; cin >> n;
+    long long a[n];
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++){
+        if (a[i] <= 10){
+            cout << "A["<< i <<"]" << " = " << a[i] << endl;
+        }
+    }
+    return 0;
+}
