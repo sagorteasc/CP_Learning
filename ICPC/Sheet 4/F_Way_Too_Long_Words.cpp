8ffb98856d92f791c3184724302d4fd37bd3490b
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2025-08-15 21:40:26 +0600

    Sheet 4 - F_Way_Too_Long_Words

---
+++ ICPC/Sheet 4/F_Way_Too_Long_Words.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int t; cin >> t;
+    while (t--){
+        string s; cin >> s;
+        if (s.length() <= 10){
+            cout << s << endl;
+        }
+        else{
+            cout << s.front() << s.length() - 2 << s.back() << endl;
+        }
+    }
+    return 0;
+}
